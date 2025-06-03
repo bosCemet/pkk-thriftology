13312113b91b3e388f89b4aa4ea2ee0e9a1f8ffb
bosCemet <ekalayakemal@gmail.com>
2025-06-03 10:44:40 +0700

    add profile

--- public/js/burger-menu.js
+++
@@ -1,8 +0,0 @@
-const burgerBtn = document.getElementById('burgerBtn');
-const mobileMenu = document.getElementById('mobileMenu');
-
-burgerBtn.addEventListener('click', function() 
-{
-    mobileMenu.classList.toggle('hidden');
-}
-);
